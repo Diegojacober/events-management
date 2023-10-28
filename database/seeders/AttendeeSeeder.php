ea76eef2e67318fa5bc44bd9782e9e77f1f6f08d
Diego <diegoalencar.jacober@gmail.ocm>
2023-10-27 21:55:06 -0300

    Creating factory and seeders

---
+++ database/seeders/AttendeeSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Attendee;
+use App\Models\Event;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AttendeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::all();
+        $events = Event::all();
+
+        foreach ($users as $user) {
+            $eventesToAttend = $events->random(rand(1, 3));
+
+            foreach ($eventesToAttend as $event) {
+                Attendee::create([
+                    'user_id' => $user->id,
+                    'event_id' => $event->id
+                ]);
+            }
+        }
+    }
+}
