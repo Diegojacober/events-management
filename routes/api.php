f1855cb974b119abf48b04b9eb482aec586b4d07
Diego <diegoalencar.jacober@gmail.ocm>
2023-10-29 20:59:27 -0300

    Setting up authenticatin using sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,18 @@
 <?php
 
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('events', EventController::class);
-Route::apiResource('events.attendees', AttendeeController::class)
-    ->scoped(/*['attendee' => 'event']*/)->except(['update']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)
+    ->scoped(/*['attendee' => 'event']*/)->except(['update']);
